5054da1da11c7b28190a5d23c3232befd8aed2e7
Milán Péter Novák <milan.novak@stud.uni-corvinus.hu>
2021-12-10 22:56:08 +0100

    új generáció

--- evolucios_algoritmus/Form1.cs
+++ evolucios_algoritmus/Form1.cs
@@ -37,11 +37,6 @@ namespace evolucios_algoritmus
                 gc.AddPlayer(nbrOfSteps);
             }
             gc.Start();
-
-            var playerList = from p in gc.GetCurrentPlayers()
-                             orderby p.GetFitness() descending
-                             select p;
-            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
         private void Gc_GameOver(object sender)
         {
@@ -49,6 +44,27 @@ namespace evolucios_algoritmus
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
